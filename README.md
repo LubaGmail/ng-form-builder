e7606046dd4953ac9803abe3c5d52be948567825
luba.powell <powe2009@gmail.com>
2019-06-25 18:22:08 -0400

    ng form builder

---
+++ README.md
@@ -0,0 +1,29 @@
+
+https://codewithmosh.com/courses/206545/lectures/3196339
+
+ng new authors
+
+ng g c authors
+ng g s authors
+ng g c contact-form
+
+ng serve            -- localhost:4200
+
+Git
+    rm -rf .git
+    git push -u origin master
+
+npm i bootstrap --save
+npm i font-awesome --save
+
+label+input[type='text'].form-control   
+
+style.css
+    @import '~bootstrap/dist/css/bootstrap.css';
+    @import "~font-awesome/css/font-awesome.css";
+
+zen coding
+    ul>li       + Tab
+
+Cntl .          - import the focused entity
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent,
+    NewCourseFormComponent,
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
